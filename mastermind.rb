af8a7fa2c36e74f78ff7e6cd76c7c731554f6e66
Rathersilly <rathersilly@live.com>
2017-05-19 21:03:03 -0600

    can determine won game

--- mastermind.rb
+++ mastermind.rb
@@ -4,8 +4,10 @@
 # player
 
 #store code and guesses as arrays
-class Game
 
+
+class Game
+  MAX_TRIES = 4
 
   def initialize(code_length = 4, code_base = 6)
     #@code_maker = code_maker
@@ -14,6 +16,8 @@ class Game
     @code_length = code_length
     @code_base = code_base     #number of possible elements in code
     @guesses = []
+    @returns = []
+    @tries = 0
 
     #board = Board.new(code_length, code_base)
   end
@@ -26,21 +30,32 @@ class Game
 
     #
     input = gets.chomp
-    @code = input.split("")
+    @code = input.split("")     #code is an array of strings
 
     #while code not guessed and tries not exceeded
-    puts "OK now other guy guess code"
-    puts "Format: 'ABCD' if guess ABCD"
-    input = gets.chomp
-    guess = input.split("")
-    @guesses.push(guess)
+    while @tries < MAX_TRIES
+      puts "OK now other guy guess code"
+      puts "Format: 'ABCD' if guess ABCD"
+      input = gets.chomp
+      guess = input.split("")     #guess is an array
+      #puts guess
+      @guesses.push(guess)        #@guesses is an array of arrays of strings
+      
+      #puts "@guesses: #{@guesses}"
+      @tries += 1
 
-    guess_result = check_guess(guess)
+      guess_result = check_guess(guess)
+      @returns.push(guess_result)
 
-    #compare code and guess and return key pegs
-    if guess_result[:all_correct] = code_length
-      #game over with win
+      draw_board
+
+      
+      if guess_result[:all_correct] == @code_length
+        puts "WIN"
+        #game over with win
+      end
     end
+
 
     #show 
   end
@@ -48,8 +63,11 @@ class Game
   private
 
   def check_guess(guess)
-    code_temp = @code
-    # puts "code_temp = #{code_temp}"
+    code_temp = @code.dup
+    
+    guess_temp = guess.dup
+    
+        # puts "code_temp = #{code_temp}"
     # puts "guess = #{guess}"
     
     return_hash = {all_correct: 0, position_only: 0}
@@ -63,23 +81,27 @@ class Game
       # p guess[i]
       # p code_temp[i]
       # p return_hash[:all_correct]
-      if guess[i-correct] == code_temp[i-correct]
+      if guess_temp[i-correct] == code_temp[i-correct]
         return_hash[:all_correct] += 1
         code_temp.delete_at(i-correct)
-        guess.delete_at(i-correct)
+        guess_temp.delete_at(i-correct)
         correct += 1
       end
       
-      puts "code_temp = #{code_temp}"
-      puts "guess = #{guess}"
-      p return_hash
-      puts
+      # puts "code_temp = #{code_temp}"
+      # puts "guess_temp = #{guess_temp}"
+      # p return_hash
+      # puts
     end
+
     code_temp.each_with_index do  |x, i|
-      if guess.include?(x)
-        guess.delete(x)
+      if guess_temp.include?(x)
+        # puts "\n\n guesses"
+        # p @guesses134
+        # puts "\n\n"
+        guess_temp.delete(x)
         code_temp[i] = " "
-        puts "match found"
+        # puts "match found"
         
         return_hash[:position_only] += 1
 
@@ -88,14 +110,18 @@ class Game
     end
 
     
-    puts return_hash
+    #puts return_hash
     return return_hash
   end
 
   def draw_board
-    @guesses.each do |x|
-      puts x
+    puts "X" * @code_length + "\tall_correct\tposition_only"
+    
+    @guesses.each_with_index do |x, i|
+      print x.join + "\t#{@returns[i][:all_correct]}\t\t#{@returns[i][:position_only]}"
+      puts
     end
+    puts
   end
 end
 
